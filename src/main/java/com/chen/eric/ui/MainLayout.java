0b37f08a5d794dfe982b0e9cf0110658dca71e99
Zeming <chenz16@rose-hulman.edu>
2020-02-11 00:50:26 -0500

    add version1 release war

--- src/main/java/com/chen/eric/ui/MainLayout.java
+++ src/main/java/com/chen/eric/ui/MainLayout.java
@@ -22,6 +22,7 @@ import com.chen.eric.ui.views.ContainerView;
 import com.chen.eric.ui.views.CustomerView;
 import com.chen.eric.ui.views.EmployeeView;
 import com.chen.eric.ui.views.Home;
+import com.chen.eric.ui.views.Statistics;
 import com.chen.eric.ui.views.VesselView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasElement;
@@ -133,7 +134,7 @@ public class MainLayout extends FlexBoxLayout
 	private void initNaviItems() {
 		NaviMenu menu = naviDrawer.getMenu();
 		//menu.addNaviItem(VaadinIcon.HOME, "Home", Home.class);
-		//menu.addNaviItem(VaadinIcon.INSTITUTION, "Accounts", Accounts.class);
+		menu.addNaviItem(VaadinIcon.CHART, "Accounts", Statistics.class);
 		menu.addNaviItem(VaadinIcon.BOAT, "Vessel", VesselView.class);
 		menu.addNaviItem(VaadinIcon.CUBES, "Container Location", ContainerLocationView.class);
 		menu.addNaviItem(VaadinIcon.PACKAGE, "Container", ContainerView.class);
